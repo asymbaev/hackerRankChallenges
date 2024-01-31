47426d2e5a6e7ad7aff678c94529be8cf449f6c4
Dastan <asymbaevdastan07@gmail.com>
2024-01-31 20:27:38 +0600

    cmpltd

--- src/main/java/org/example/hackerRankProblems/CompareTriplets.java
+++ src/main/java/org/example/hackerRankProblems/CompareTriplets.java
@@ -1,4 +1,21 @@
 package org.example.hackerRankProblems;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CompareTriplets {
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int aliceScore = 0;
+        int bobScore = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > b.get(i)) {
+                aliceScore++;
+            } else if (a.get(i) < b.get(i)) {
+                bobScore++;
+                
+            }
+        }
+        return Arrays.asList(aliceScore, bobScore);
+    }
 }
