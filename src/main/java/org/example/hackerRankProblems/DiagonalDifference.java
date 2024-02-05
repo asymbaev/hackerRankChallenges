81712d7c7cdd80b09c50793c82c5c6a9c68c1bc6
Dastan <asymbaevdastan07@gmail.com>
2024-02-05 21:51:58 +0600

    sum of diagonals

--- src/main/java/org/example/hackerRankProblems/DiagonalDifference.java
+++ src/main/java/org/example/hackerRankProblems/DiagonalDifference.java
@@ -1,4 +1,14 @@
 package org.example.hackerRankProblems;
 
+import java.util.List;
+
 public class DiagonalDifference {
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int n = arr.size();
+        int sumOfLeft = 0;
+        int sumOfRight = 0;
+
+
+    }
 }
