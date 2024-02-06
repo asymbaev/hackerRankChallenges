5d6d938623db9ae5e9df01efa4a0dd5fb5e96086
Dastan <asymbaevdastan07@gmail.com>
2024-02-06 17:33:16 +0600

    solved the problem

--- src/main/java/org/example/hackerRankProblems/PlusMinus.java
+++ src/main/java/org/example/hackerRankProblems/PlusMinus.java
@@ -1,4 +1,29 @@
 package org.example.hackerRankProblems;
 
+import java.util.List;
+
 public class PlusMinus {
+    public static void plusMinus(List<Integer> arr) {
+        int len = arr.size();
+
+        float positiveCount = 0;
+        float negativeCount = 0;
+        float zeroCount = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (arr.get(i) > 0) {
+                positiveCount++;
+            } else if (arr.get(i) < 0) {
+                negativeCount++;
+                
+            } else if (arr.get(i) == 0) {
+                zeroCount++;
+
+            }
+        }
+
+        System.out.printf("%.6f\n", positiveCount / len);
+        System.out.printf("%.6f\n", negativeCount / len);
+        System.out.printf("%.6f\n", zeroCount / len);
+    }
 }
