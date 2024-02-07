5e5c7fb24d616b27676ca1e3b05877ba807e6685
Dastan <asymbaevdastan07@gmail.com>
2024-02-07 23:33:56 +0600

    Solved a problem

--- src/main/java/org/example/hackerRankProblems/BirthdayCandles.java
+++ src/main/java/org/example/hackerRankProblems/BirthdayCandles.java
@@ -1,4 +1,20 @@
 package org.example.hackerRankProblems;
 
+import java.util.Collections;
+import java.util.List;
+
 public class BirthdayCandles {
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        int maxHeight = Collections.max(candles);
+
+        int count = 0;
+
+        for (int candle: candles) {
+            if (candle == maxHeight) {
+                count++;
+            }
+        }
+        return count;
+
+    }
 }
