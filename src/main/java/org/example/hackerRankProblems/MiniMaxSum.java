ce8501826730229807ae1a2adb3e0bf0de43f37e
Dastan <asymbaevdastan07@gmail.com>
2024-02-07 22:00:47 +0600

    solved the problem

--- src/main/java/org/example/hackerRankProblems/MiniMaxSum.java
+++ src/main/java/org/example/hackerRankProblems/MiniMaxSum.java
@@ -1,4 +1,22 @@
 package org.example.hackerRankProblems;
 
+import java.util.Collections;
+import java.util.List;
+
 public class MiniMaxSum {
+    public static void miniMaxSum(List<Integer> arr) {
+        Collections.sort(arr);
+
+        long minSum = 0;
+        long maxSum = 0;
+
+        // Calculate minimum sum
+        for (int i = 0; i < 4; i++) {
+            minSum += arr.get(i);
+        }
+        for (int i = arr.size() - 1; i >= arr.size() - 4; i--) {
+            maxSum += arr.get(i);
+        }
+        System.out.println(minSum + " " + maxSum);
+    }
 }
