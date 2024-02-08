d67739eb80c16c32693da10150b919255e4f0038
Dastan <asymbaevdastan07@gmail.com>
2024-02-08 19:13:05 +0600

    Solved the problem

--- src/main/java/org/example/hackerRankProblems/GradingStudents.java
+++ src/main/java/org/example/hackerRankProblems/GradingStudents.java
@@ -1,4 +1,17 @@
 package org.example.hackerRankProblems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GradingStudents {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> roundedGrades = new ArrayList<>();
+
+        for(int grade: grades) {
+            int roundedGrade = (grade < 39 || grade % 5 < 3) ? grade : ((grade / 5) * 5) + 5;
+            roundedGrades.add(roundedGrade);
+        }
+        return roundedGrades;
+    }
 }
+
