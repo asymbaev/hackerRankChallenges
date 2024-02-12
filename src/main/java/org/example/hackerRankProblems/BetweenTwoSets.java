e47b5113a12f577bd771855b596cd0f6e1445c93
Dastan <asymbaevdastan07@gmail.com>
2024-02-12 14:14:47 +0600

    solved the problem

--- src/main/java/org/example/hackerRankProblems/BetweenTwoSets.java
+++ src/main/java/org/example/hackerRankProblems/BetweenTwoSets.java
@@ -1,4 +1,36 @@
 package org.example.hackerRankProblems;
 
+import java.util.Collections;
+import java.util.List;
+
 public class BetweenTwoSets {
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        int count = 0;
+        int maxA = Collections.max(a);
+        int minB = Collections.min(b);
+
+        for(int i = maxA; i<=minB; i++) {
+            boolean isFactorOfB = true;
+            for(int num : b) {
+                if(num % i != 0) {
+                    isFactorOfB = false;
+                    break;
+                }
+            }
+            if(isFactorOfB) {
+                boolean isFactorOfA = true;
+                for(int num : a) {
+                    if(i % num != 0) {
+                        isFactorOfA = false;
+                        break;
+                    }
+                }
+                if (isFactorOfA) {
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
 }
