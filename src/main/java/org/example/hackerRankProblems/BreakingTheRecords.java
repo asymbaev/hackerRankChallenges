863f5cf0057093e4dd3663cd1f0dcd2b499a13d6
Dastan <asymbaevdastan07@gmail.com>
2024-02-12 16:00:10 +0600

    Solved a problem

--- src/main/java/org/example/hackerRankProblems/BreakingTheRecords.java
+++ src/main/java/org/example/hackerRankProblems/BreakingTheRecords.java
@@ -1,4 +1,34 @@
 package org.example.hackerRankProblems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BreakingTheRecords {
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+
+        int lowestScore = scores.get(0);
+        int highestScore = scores.get(0);
+
+        int countLowest = 0;
+        int countHighest = 0;
+
+        List<Integer> recordCounts = new ArrayList<>();
+
+        for(int i = 1; i < scores.size(); i++) {
+            int currentScore = scores.get(i);
+
+            if (currentScore < lowestScore) {
+                lowestScore = currentScore;
+                countLowest++;
+            }
+            if (currentScore > highestScore) {
+                highestScore = currentScore;
+                countHighest++;
+            }
+        }
+        recordCounts.add(countHighest);
+        recordCounts.add(countLowest);
+
+        return recordCounts;
+    }
 }
