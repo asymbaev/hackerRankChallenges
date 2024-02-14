8dc3437febe2efb6e09c140673463012dab928bc
Dastan <asymbaevdastan07@gmail.com>
2024-02-14 12:58:42 +0600

    Solved the problem

--- src/main/java/org/example/hackerRankProblems/SubArrayDivision.java
+++ src/main/java/org/example/hackerRankProblems/SubArrayDivision.java
@@ -16,5 +16,21 @@ public class SubArrayDivision {
         for (int i = 0; i < m; i++) {
             sum += s.get(i);
         }
+        // Check if the sum of the first Window equals d
+
+        if(sum == d) {
+            count++;
+        }
+
+        // Slide the window and check subsequent arrays.
+        for(int i = m; i<s.size(); i++) {
+            // Add the current element to the sum and remove leftmost element from the window.
+            sum += s.get(i) - s.get(i-m);
+            // Check if the sum equals d
+            if (sum == d) {
+                count++;
+            }
+        }
+        return count;
     }
 }
