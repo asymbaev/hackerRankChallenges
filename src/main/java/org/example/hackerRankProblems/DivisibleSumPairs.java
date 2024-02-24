ab4181f7f5583a5a2a302df647d4f401a0b3b9d5
Dastan <asymbaevdastan07@gmail.com>
2024-02-24 13:53:10 -0500

    Solved the problem

--- src/main/java/org/example/hackerRankProblems/DivisibleSumPairs.java
+++ src/main/java/org/example/hackerRankProblems/DivisibleSumPairs.java
@@ -1,4 +1,28 @@
 package org.example.hackerRankProblems;
 
+import java.util.List;
+
 public class DivisibleSumPairs {
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        int count = 0;
+
+        for(int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if((ar.get(i) + ar.get(j)) % k == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
